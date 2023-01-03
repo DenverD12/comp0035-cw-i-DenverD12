b05a82a060957fe33761aedc2e52a712631be71c
Denver D'Silva <denverd245@gmail.com>
2023-01-03 18:44:53 +0000

    folder: moved conftest.py outside test folder

---
+++ coursework2/conftest.py
@@ -0,0 +1,27 @@
+import decimal
+import pytest
+from test.shopping_basket import Basket
+from test.shopping_basket import Item
+
+
+@pytest.fixture(scope='session')
+def basket():
+    basket = Basket()
+    return basket
+
+
+@pytest.fixture(scope='session')
+def example_item_toastie():
+    example_item_toastie = Item(
+        brand_name='Warburtons', product_name='Toastie',
+        description='800g white sliced loaf', price=decimal.Decimal('1.52'))
+    yield example_item_toastie
+
+
+@pytest.fixture(scope='session')
+def example_item_butter():
+    example_item_butter = Item(
+        brand_name='Flora', product_name='Buttery',
+        description='Buttery spread', price=decimal.Decimal('0.89')
+        )
+    yield example_item_butter
