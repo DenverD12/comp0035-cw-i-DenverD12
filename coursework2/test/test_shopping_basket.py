65d21081dd167ef613a4bf578c340698a4a8acf1
Denver D'Silva <denverd245@gmail.com>
2023-01-03 16:17:29 +0000

    test: created 1 test for adding item add_item()

--- coursework2/test/test_shopping_basket.py
+++ coursework2/test/test_shopping_basket.py
@@ -33,3 +33,32 @@ def test_reset(basket, example_item_toastie):
     ])
     # Uses the fixtures (created in conftest) passed as a string as parameters
     # Uses the item quantity a parameter
+def test_when_add_item_then_basket_contains_item(
+        basket, example_item_name, quantity, request):
+    """
+    Tests if the add_item() function adds the item successfully to
+    the basket by checking if is a key in the basket dictionary.
+
+    Args:
+        basket: fixture of the shopping Basket dictionary
+        example_item_name: parameter containing fixtures of 2 example items
+        quantity: parameter containing quantity of each example item
+        request: built in pytest fixture to obtain the value of the fixtures
+
+
+    Given: An item/items (created as a fixtures in conftest.py)
+    When: The item/s are added to the basket using add.item()
+    Then: The item/s should be in the basket (dictionary)
+
+    """
+    # Reset basket for each test iteration
+    basket.reset()
+    # Obtain value of each of the fixtures such as brand_name, price
+    example_item_name = request.getfixturevalue(example_item_name)
+
+    # Add item to basket for each parameterized case
+    basket.add_item(example_item_name, quantity)
+
+    # Assert if the item for each case is in the basket dictionary
+    assert example_item_name in basket.items
+
