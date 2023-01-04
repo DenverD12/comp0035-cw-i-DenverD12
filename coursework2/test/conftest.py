25fa1c00fd4cc9c6056294b99c270c598cefbcbb
Denver D'Silva <denverd245@gmail.com>
2023-01-04 02:34:15 +0000

    test: added docstrings to conftest.py fixtures

--- coursework2/test/conftest.py
+++ coursework2/test/conftest.py
@@ -6,12 +6,21 @@ from coursework2.test.shopping_basket import Item
 
 @pytest.fixture(scope='session')
 def basket():
+    """
+    Creates a basket dictionary as a fixture
+    Uses function Basket() imported from the file to be tested:
+    shopping_basket.py
+    """
     basket = Basket()
     return basket
 
 
 @pytest.fixture(scope='session')
 def example_item_toastie():
+    """
+    Creates an item, as a fixture using the Item() function
+    from the file to be tested: shopping_basket.py
+    """
     example_item_toastie = Item(
         brand_name='Warburtons', product_name='Toastie',
         description='800g white sliced loaf', price=decimal.Decimal('1.52'))
@@ -20,6 +29,10 @@ def example_item_toastie():
 
 @pytest.fixture(scope='session')
 def example_item_butter():
+    """
+    Creates another item using the Item() function in the file to be tested:
+    shopping_basket.py
+    """
     example_item_butter = Item(
         brand_name='Flora', product_name='Buttery',
         description='Buttery spread', price=decimal.Decimal('0.89')
